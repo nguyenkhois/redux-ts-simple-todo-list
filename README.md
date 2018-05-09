4c28520875158873f864ef642155f20018ef0c38
Khoi Le <minarek@outlook.com>
2018-05-09 14:01:45 +0200

    Refactored from pure JS to TypeScript

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# redux-ts-simple-todo-list
-A simple to-do list based on Redux and TypeScript
+# Redux-TypeScript - Simple To-Do list
+The simple Redux-TypeScript web application. ([View online demo](https://nguyenkhois.github.io/redux-ts-simple-todo-list/dist/))
+
+## Screenshot
+![Screenshot](src/screenshot.png)
+
+## Compatibility
+Build environment is compatible with:
+* Windows
+* MacOS
+* Debian
+
+Webpack Dev Server reload automatically your web browser whenever a change is made to one of your files.
+
+## Installation
+* Run `npm install` to install all needed packages
+
+## Using
+* Run `npm start` or `npm run start` to start your local web server at `localhost:9000`
+* Run `npm run build` to build your *.js files to `dist` folder
+* Run `npm run build:all` to build all your files (*.js and styles.css) with `--mode production` to `dist` folder
+
+## References
+* [Migrating from JavaScript](https://www.typescriptlang.org/docs/handbook/migrating-from-javascript.html)
+* [Leveling Up with React: Redux](https://css-tricks.com/learning-react-redux/)
+* [Non-mutating methods](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype#Accessor_methods)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackNotifierPlugin = require("webpack-notifier");
+
+module.exports = {
+    devtool: 'source-map',
+    mode: 'development', // whether we're building for dev or prod
+    entry: './src/index.ts',// which file to begin with, 
+    output: {
+        path: path.resolve(__dirname, "./dist"), // what folder to put bundle in
+        filename: 'bundle.js' // what name to use for bundle
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({'process.env.NODE_ENV':JSON.stringify('production')}),
+        new WebpackNotifierPlugin({alwaysNotify: true})
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "./dist"), // the root for the server
+        watchContentBase: true, // so we reload if other stuff like CSS changes
+        port: 9000, // it'll now be at http://localhost:9000
+        watchOptions: {
+            ignored: /node_modules/
+        }
+    }
+};
